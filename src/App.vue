6a2782ad00c31b7695b673e0988712fdaac1358f
parthbhodia09 <pbhodia1@umbc.edu>
2023-09-23 18:19:49 -0400

    Init repo

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="app">
+		<!-- <SearchBarComponent @search="performSearch" /> -->
+		<!-- <FiltersComponent @filter="applyFilters" /> -->
+		<MapComponent :movies="filteredMovies" />
+	</div>
+</template>
+
+<script>
+// import SearchBarComponent from "./components/SearchBarComponent.vue";
+// import FiltersComponent from "./components/FiltersComponent.vue";
+import MapComponent from "./components/MapComponent.vue";
+import moviesData from "./assets/moviesData.json";
+
+export default {
+	components: {
+		// SearchBarComponent,
+		// FiltersComponent,
+		MapComponent,
+	},
+	data() {
+		return {
+			movies: moviesData,
+			filteredMovies: moviesData,
+			searchQuery: "",
+			filters: {},
+		};
+	},
+	methods: {
+		performSearch(query) {
+			this.searchQuery = query;
+			this.updateFilteredMovies();
+		},
+		applyFilters(filters) {
+			this.filters = filters;
+			this.updateFilteredMovies();
+		},
+		updateFilteredMovies() {
+			// Your logic here to filter movies based on searchQuery and filters
+		},
+	},
+};
+</script>
+
+<style>
+/* Add your global styles here */
+</style>
---
+++ src/components/MapComponent.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-container>
+		<div id="map"></div>
+	</v-container>
+</template>
+
+<script>
+import L from "leaflet";
+
+export default {
+	mounted() {
+		// Initialize the map
+		var map = L.map("map").setView([51.505, -0.09], 13);
+
+		// Set up the tile layer
+		L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			attribution: "Map data &copy; OpenStreetMap contributors",
+		}).addTo(map);
+	},
+};
+</script>
+
+<style>
+#map {
+	height: 500px;
+}
+</style>
